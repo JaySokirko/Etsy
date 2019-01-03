49a21e10fe3a30c0b0cfe92cd7f0d830bb2da82b
JaySokirko <sokirko0601@gmail.com>
2019-01-03 15:54:01 +0200

    master

--- app/src/main/java/com/jay/foxmindtest/view/activity/DetailedScreenActivity.java
+++ app/src/main/java/com/jay/foxmindtest/view/activity/DetailedScreenActivity.java
@@ -1,10 +1,15 @@
 package com.jay.foxmindtest.view.activity;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.jay.foxmindtest.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -12,13 +17,45 @@ public class DetailedScreenActivity extends AppCompatActivity {
 
     private String TAG = "LOG_TAG";
 
+    private String image;
+    private String title;
+    private String description;
+    private String price;
+    private String currencyCode;
+
+    private boolean isAlReadySaved;
+
+    private FrameLayout parentLayout;
+    private FloatingActionButton saveBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_screen);
 
+        parentLayout = findViewById(R.id.parent_layout);
+        saveBtn = findViewById(R.id.save);
 
-        Log.d(TAG, "onCreate: " + getIntent().getStringExtra("title"));
+        isAlReadySaved = false;
 
+        image = getIntent().getStringExtra("image");
+        title = getIntent().getStringExtra("title");
+
+        description = getIntent().getStringExtra("description");
+
+        price = getIntent().getStringExtra("price");
+        currencyCode = getIntent().getStringExtra("currency");
+
+        ImageView imageView = findViewById(R.id.image);
+        TextView titleTextView = findViewById(R.id.title);
+        TextView descriptionTextView = findViewById(R.id.description);
+        TextView priceTextView = findViewById(R.id.price);
+        TextView currencyTextView = findViewById(R.id.currency);
+
+        Picasso.get().load(image).into(imageView);
+        titleTextView.setText(title);
+        descriptionTextView.setText(description);
+        priceTextView.setText(getResources().getString(R.string.price).concat(price));
+        currencyTextView.setText(getResources().getString(R.string.currency_code).concat(currencyCode));
     }
 }
